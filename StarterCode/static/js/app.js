8fdeaa842448cb90094df72b79ea40f470dd80f4
Jennifer White <144621532+jnnfrwht14@users.noreply.github.com>
2024-01-11 22:49:59 -0600

    01.11 deltas

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,23 @@
+//Use d3 to select the data
+const bbUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+const bellyButton = d3.json(bbUrl);
+console.log("Belly Button: ", bellyButton);
+
+// Fetch the JSON data and console log it
+d3.json(bbUrl).then(function(data){console.log(data)});
+
+//trace for the belly button data
+let trace1 = {
+    x:
+    y:
+    type: "bar"
+};
+
+//data trace array
+let data = [trace1];
+
+let layout = {
+    title: "top 10 OTUs found"
+};
+Plotly.newPlot("plot", data, layout);
