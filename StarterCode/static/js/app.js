ea82c9502e18495fa882d1bae5992f0a29b73f0b
Jennifer White <144621532+jnnfrwht14@users.noreply.github.com>
2024-01-16 23:03:10 -0600

    0116 deltas

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,13 +5,15 @@ const bbUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroo
 d3.json(bbUrl).then(function(data){console.log(data)});
 
 function init() {
-   let dropdownMenu = d3.selectAll("#selectDataSet");
+   let dropdownMenu = d3.select("#selDataSet");
    
    d3.json(bbUrl).then((data) => {
-        let names = data.names;
+        
+    let names = data.names;
         
         names.forEach((id) => {
             console.log(id);
+            
             dropdownMenu.append('option')
             .text(id)
             .property('value', id);
@@ -23,35 +25,38 @@ function init() {
         buildMetaData(picksample);
         buildBarChart(picksample);
         buildBubbleChart(picksample);
-        
-        // Call updatePlotly() when a change takes place to the DOM
-        d3.selectAll("#selectDataset").on("change", updatePlotly);
 
-        // This function is called when a dropdown menu item is selected
-        function updatePlotly() {
-        // Use D3 to select the dropdown menu
-        let dropdownMenu = d3.select("#selectDataset");
-        // Assign the value of the dropdown menu option to a variable
-        let dataset = dropdownMenu.property('value');
-        };
+        var selectDropdown = d3.select("#selDataset");
+
+    function addOptions() {
+       d3.json(bbUrl).then(function(data) {
+        
+            data.names.forEach((name, i) => {
+                var onChange = selectDropdown.append("option").text(name).attr('value', i);
+            });
+        });
+    }
+        
+    addOptions();
     });
    };
-    
-
+   
     function buildMetaData(sample) {
        
         d3.json(bbUrl).then((data) => {
            
             let metadata = data.metadata;
             let value = metadata.filter(result => result.id == sample);
+
             console.log(value)
         
             let valueData = value[0];
+            console.log(value[2])
+            d3.select("#sample-metadata").html("");
 
-            d3.select('#sample-metadata').html('');
             Object.entries(valueData).forEach(([key, value]) => {
-                console.log(key, value);
-                d3.select('#sample-metadata').append('h5').text(`${key}: ${value}`);
+                d3.select("#sample-metadata").append('h6').text(`${key}: ${value}`);
+                console.log(Object.keys(value));
             });
         });
     };
@@ -68,6 +73,7 @@ function init() {
 
                 console.log(otu_ids, otu_labels, sample_values);
 
+                //select top 10 items
                 let xticks = sample_values.slice(0,10).reverse();
                 let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
                 let labels = otu_labels.slice(0,10).reverse();
@@ -85,7 +91,7 @@ function init() {
                 };
 
                 let layout = {
-                     title: "Top 10 OTUs Present",
+                     title: "Top 10 Operational Taxonomic Units (OTUs) Present",
                      hovermode: otu_labels
                 };
                 // Call Plotly to plot the bar chart
@@ -121,7 +127,7 @@ function init() {
             let layout = {
                 title: "Bacteria per Sample",
                 hovermode: 'closest',
-                xaxis: {title: "OTU IDs"},
+                xaxis: {title: "Operational Taxonomic Units (OTU) IDs"},
             };
             // Call Plotly to plot the bubble chart
             Plotly.newPlot('bubble', [trace2], layout)
@@ -129,12 +135,12 @@ function init() {
         });
     };
 
-    function dropdownMenuChange(value) {
+    function optionChanged(value) {
         console.log(value);
 
         buildMetaData(value);
         buildBarChart(value);
         buildBubbleChart(value);
     };
-  
+     
   init();
